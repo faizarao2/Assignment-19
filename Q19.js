300e833911e502e2ac565bb80be1a3b49eec4ad3
Faiza Rao <159902088+faizarao2@users.noreply.github.com>
2024-02-21 19:40:00 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,7 @@
+"use strict";
+// 19.Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Define an array of places to visit
+let placesToVisit = ["Dubai", "Paris", "New York", "Makkah", "Burjh Khalifa"];
+// Print the number of dinner guests
+console.log(`Number of dinner guests: ${placesToVisit.length}`);
